3ce9855283f05de14f1c95ef8c32fdbeda194cbc
Ankit Singh <122378526+ankitsingh1421@users.noreply.github.com>
2025-03-29 00:19:43 +0530

    Update Navbar.tsx

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,9 @@
-
 import React, { useState, useEffect } from "react";
 import { Link as ScrollLink } from "react-scroll";
 import { useUser, SignInButton, SignOutButton, useClerk, SignedOut, SignedIn, UserButton } from "@clerk/clerk-react";
 import WhatsAppButton from "../whatsapp/WhatsAppButton";
 import { Link } from "react-router-dom";
+import { BriefcaseIcon, Heart } from "lucide-react";
 
 export function Navbar() {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -11,7 +11,6 @@ export function Navbar() {
   const { user, isSignedIn } = useUser();
   const { signOut } = useClerk();
 
-  // Add scroll effect to change background on scroll
   useEffect(() => {
     const handleScroll = () => setIsScrolled(window.scrollY > 0);
     window.addEventListener("scroll", handleScroll);
@@ -24,17 +23,17 @@ export function Navbar() {
         }`}
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-       
+
         <div className="flex items-center justify-between h-20">
           {/* Logo */}
           <Link to='/'>
-          <div className="flex items-center space-x-2">
-            <img src="/logoNav.png" className="w-14 h-16" alt="Logo" />
-            <span className="text-white text-xl font-bold">
-              Decoding <br /> Bollywood Hits
-            </span>
-          </div>
-        </Link>
+            <div className="flex items-center space-x-2">
+              <img src="/logoNav.png" className="w-14 h-16" alt="Logo" />
+              <span className="text-white text-xl font-bold">
+                Decoding <br /> Bollywood Hits
+              </span>
+            </div>
+          </Link>
           {/* Desktop Navigation */}
           <div className="hidden md:flex items-center space-x-8">
             <ScrollLink
@@ -100,7 +99,21 @@ export function Navbar() {
                         avatarBox: "w-10 h-10",
                       },
                     }}
-                  />
+                  >
+                   <UserButton.MenuItems>
+                <UserButton.Link
+                  label="My courses"
+                  labelIcon={<BriefcaseIcon size={15} />}
+                  href="/my-course"
+                />
+                <UserButton.Link
+                  label="Saved courses"
+                  labelIcon={<Heart size={15} />}
+                  href="/saved-course"
+                />
+                <UserButton.Action label="manageAccount" />
+              </UserButton.MenuItems>
+             </UserButton>
                 </div>
               </SignedIn>
             </div>
@@ -124,14 +137,12 @@ export function Navbar() {
         className={`absolute top-0 left-0 w-full h-screen bg-navy-950/95 backdrop-blur-lg z-40 transform transition-transform duration-300 ${isMenuOpen ? "translate-x-0" : "-translate-x-full"
           } md:hidden`}
       >
-        <Link to='/'>
         <div className="flex items-center space-x-2 mt-3 ml-2">
           <img src="/logoNav.png" className="w-14 h-16" alt="Logo" />
           <span className="text-white text-xl font-bold">
             Decoding <br /> Bollywood Hits
           </span>
         </div>
-        </Link>
         <div style={{ marginLeft: "30px", marginTop: "20px" }}>
           <SignedIn>
             <div className="flex items-center space-x-4">
@@ -139,13 +150,27 @@ export function Navbar() {
                 Welcome, {user?.firstName}
               </span>
               <UserButton
-                afterSignOutUrl="/"
-                appearance={{
-                  elements: {
-                    avatarBox: "w-10 h-10",
-                  },
-                }}
-              />
+                    afterSignOutUrl="/"
+                    appearance={{
+                      elements: {
+                        avatarBox: "w-10 h-10",
+                      },
+                    }}
+                  >
+                   <UserButton.MenuItems>
+                <UserButton.Link
+                  label="My courses"
+                  labelIcon={<BriefcaseIcon size={15} />}
+                  href="/my-course"
+                />
+                <UserButton.Link
+                  label="Saved courses"
+                  labelIcon={<Heart size={15} />}
+                  href="/saved-course"
+                />
+                <UserButton.Action label="manageAccount" />
+              </UserButton.MenuItems>
+             </UserButton>
             </div>
           </SignedIn>
         </div>
